5ef2fe408f3475d5c4c8e53266f82a36710db65d
diegok88 <ddkotz@minha.fag.edu.br>
2026-08-02 06:33:49 -0300

    prisma service allmodels audit

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,18 +6,13 @@ import { UpdateAuditoriaDto } from '@/modules/auditoria/dto/update-auditoria.dto
 import { ExtractDataAuditoria } from '@/utils/extract-data-auditoria.util';
 import { TYPES_NOTICES } from '@/utils/types-notices.cosnt';
 import {
-  forwardRef,
-  Inject,
   Injectable,
-  Logger,
   NotFoundException,
   OnModuleDestroy,
   OnModuleInit,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { JsonNull } from '@prisma/client/runtime/client';
 import { Pool } from 'pg';
 
 @Injectable()
@@ -25,7 +20,6 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  private logger = new Logger(PrismaService.name);
   private auditoriaService!: AuditoriaService;
   public extended: any;
 
@@ -40,6 +34,10 @@ export class PrismaService
     super({ adapter });
   }
 
+  public get client() {
+    return this.extended || this;
+  }
+
   async onModuleInit() {
     await this.$connect();
 
@@ -48,39 +46,38 @@ export class PrismaService
     });
 
     const tenantContext = this.tenantContextService;
-
     const getAuditoriaService = () => this.auditoriaService;
-
     const prismaClient = this;
+
+    const MODELOS_AUDITADOS = ['gestor'];
 
     this.extended = this.$extends({
       query: {
-        gestor: {
+        $allModels: {
           // CRIAÇÃO - create
-          async create({ args, query }) {
-            const user = tenantContext.getStore();
-            const result = await query(args);
+          async create({ model, args, query }) {
+            const modeloNome = model.toUpperCase();
+            const usuario = tenantContext.getStore();
+            const resultado = await query(args);
 
-            if (!user) {
-              return result;
-            }
+            if (!usuario) return resultado;
+            if (!MODELOS_AUDITADOS.includes(modeloNome)) return resultado;
 
             const dados: CreateAuditoriaDto = {
-              entidade: 'GESTOR',
-              registroId: result.id!,
+              entidade: modeloNome,
+              registroId: resultado.id!,
               acao: Acao.CREATE,
-              dadosRegistrados: ExtractDataAuditoria(result),
-              empresaId: user.empresa,
-              registradoPorId: user.user,
+              dadosRegistrados: JSON.stringify(ExtractDataAuditoria(resultado)),
+              empresaId: usuario.empresa,
+              registradoPorId: usuario.user,
             };
-            console.log('Dados de auditoria:', dados);
 
             await getAuditoriaService().create(dados, prismaClient);
-
-            return result;
+            return resultado;
           },
           // ATUALIZAÇÃO UNICA - update
-          async update({ args, query }) {
+          async update({ model, args, query }) {
+            const modeloNome = model.toUpperCase();
             const user = tenantContext.getStore();
             if (!user) return query(args);
 
@@ -91,7 +88,7 @@ export class PrismaService
               delete dataAny._auditAction;
             }
 
-            const antes = await prismaClient.gestor.findUnique({
+            const antes = await prismaClient[model].findUnique({
               where: args.where,
             });
 
@@ -112,7 +109,7 @@ export class PrismaService
             return depois;
           },
           // ATUALIZAÇÃO EM LOTE - update many
-          async updateMany({ args, query }) {
+          async updateMany({ model, args, query }) {
             const user = tenantContext.getStore();
             if (!user) return query(args);
 
@@ -123,17 +120,17 @@ export class PrismaService
               delete dataAny._auditAction;
             }
 
-            const antes = await prismaClient.gestor.findMany({
+            const antes = await prismaClient[model].findMany({
               where: args.where,
             });
 
             const resultado = await query(args);
 
-            const depois = await prismaClient.gestor.findMany({
+            const depois = await prismaClient[model].findMany({
               where: args.where,
             });
 
-            const dadosAuditoria = antes.map((itemAntes) => {
+            const dadosAuditoria = antes.map((itemAntes: any) => {
               const itemDepois = depois.find((d: any) => d.id === itemAntes.id);
 
               const dados: UpdateAuditoriaDto = {
@@ -154,11 +151,11 @@ export class PrismaService
             return resultado;
           },
           // REMOÇÃO UNICA - delete
-          async delete({ args, query }) {
+          async delete({ model, args, query }) {
             const user = tenantContext.getStore();
             if (!user) return query(args);
 
-            const dados = await prismaClient.gestor.findUnique({
+            const dados = await prismaClient[model].findUnique({
               where: args.where,
             });
 
@@ -180,11 +177,11 @@ export class PrismaService
             return dados;
           },
           // REMOÇÃO EM LOTE - delete many
-          async deleteMany({ args, query }) {
+          async deleteMany({ model, args, query }) {
             const user = tenantContext.getStore();
             if (!user) return query(args);
 
-            const dados = await prismaClient.gestor.findMany({
+            const dados = await prismaClient[model].findMany({
               where: args.where,
             });
 
@@ -210,76 +207,7 @@ export class PrismaService
     });
   }
 
-  public get client() {
-    return this.extended || this;
-  }
-
   async onModuleDestroy() {
     await this.$disconnect();
   }
 }
-
-/*
- 
-    FUNÇÃO DE CRIAÇÃO UNICA DE REGISTO DE AUDITORIA:
-    - função interna.
-    - dentro da propria execução do metodo prisma.
-    
-    async function auditCreate (params: any, next: any, entidade: string) => {
-      const usuario = tenantContext.getStore();
-      const resultado = await next(params);
-      if (!usuario || !resultado) return resultado;
-
-      const dadosAuditoria: CreateAuditoriaDto = {
-        entidade: entidade,
-        registroId: resultado.id,
-        acao: Acao.CREATE,
-        dadosRegistrados: JSON.stringify(ExtractDataAuditoria(resultado)),
-        empresaId: usuario.empresa,
-        registradoPorId: usuario.user,
-      };
-
-      await getAuditoriaService().create(dadosAuditoria, prismaClient);
-
-      return resultado;
-    };
-    
-    FUNÇÃO DE ATUALIZAÇÃO UNICA DE REGISTO DE AUDITORIA:
-    - função interna.
-    - dentro da propria execução do metodo prisma.
-    
-    const auditUpdate = async (params: any, next: any, entidade: string) => {
-      const usuario = tenantContext.getStore();
-      if (!usuario) {
-        throw new UnauthorizedException(TYPES_NOTICES.INVALID_CREDENTIAL);
-      }
-
-      let acao = Acao.UPDATE;
-      const dataAny = params.args.data as any;
-      if (dataAny && dataAny._auditAction) {
-        acao = dataAny._auditAction;
-        delete dataAny._auditAction;
-      }
-
-      const antes = await prismaClient[entidade.toLowerCase()].findUnique({
-        where: params.args.where,
-      });
-
-      const depois = await next(params.args);
-
-      const dadosAuditoria: UpdateAuditoriaDto = {
-        entidade: entidade,
-        registroId: depois.id,
-        acao: acao,
-        antes: JSON.stringify(ExtractDataAuditoria(antes)),
-        depois: JSON.stringify(ExtractDataAuditoria(depois)),
-        empresaId: usuario.empresa,
-        registradoPorId: usuario.user,
-      };
-
-      await getAuditoriaService().update(dadosAuditoria, prismaClient);
-
-      return depois;
-    };
-
-*/
