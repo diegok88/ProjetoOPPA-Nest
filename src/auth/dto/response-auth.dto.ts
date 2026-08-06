17bd2096d92b577437daf6e63bf773df98186f8e
diegok88 <https://github.com/diegok88>
2026-08-05 22:20:32 -0300

    update find profile

--- src/auth/dto/response-auth.dto.ts
+++ src/auth/dto/response-auth.dto.ts
@@ -2,11 +2,9 @@ import { ResponseUsuarioDto } from '@/modules/usuario/dto/response-usuario.dto';
 import { OmitType, PickType } from '@nestjs/mapped-types';
 
 export class ResponseAuthDto extends OmitType(ResponseUsuarioDto, [
-  'id',
   'dataDesligamento',
   'senha',
   'pin',
-  'status',
 ]) {}
 
 export class ResponseAuthValidateDto extends PickType(ResponseUsuarioDto, [
